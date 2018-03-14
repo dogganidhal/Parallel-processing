2a5251b7635d27ea1a47c92499d5eed6d8c79ceb
Nidhal DOGGA <dogga.nidhal@gmail.com>
2018-03-14 16:00:29 +0100

    kotlin

--- Kotlin/src/multi-threading.kt
+++ Kotlin/src/multi-threading.kt
@@ -1,60 +1,72 @@
 var array = Array(1000000){0}
 
-fun main(args: Array<String>){
-
-    var t1 = Thread(Runnable
+fun compute_for_index(index: Int) : Int 
+{
+    var res = 0
+    for (i in 0..index)
     {
-        run {
-            for (i in 0..((array.count()/4) - 1))
-            {
-                array.set(i, i * i)
-            }
-        }
-    })
-
-    var t2 = Thread(Runnable
-    {
-        run {
-            for (i in (array.count()/4)..(2*array.count()/4) - 1)
-            {
-                array.set(i, i * i)
-            }
-        }
-    })
-
-    var t3 = Thread(Runnable
-    {
-        run {
-            for (i in (2*array.count()/4)..(3*(array.count()/4) - 1))
-            {
-                array.set(i, i * i)
-            }
-        }
-    })
-
-    var t4 = Thread(Runnable
-    {
-        run {
-            for (i in (3*array.count()/4)..(array.count() - 1))
-            {
-                array.set(i, i * i)
-            }
-        }
-    })
-
-    t1.run()
-    t2.run()
-    t3.run()
-    t4.run()
-
-    t1.join()
-    t2.join()
-    t3.join()
-    t4.join()
-
-    for (i in 0..(array.count() - 1))
-    {
-        println(array[i])
+        res += i
     }
+    return res
+}
+
+
+fun main(args: Array<String>) {
+
+    val time = measureTimeMillis({
+        var t1 = Thread(Runnable
+        {
+            run {
+                for (i in 0..((array.count()/4) - 1))
+                {
+                    array.set(i, compute_for_index(i))
+                }
+            }
+        })
+
+        var t2 = Thread(Runnable {
+            run {
+                for (i in (array.count()/4)..(2*array.count()/4) - 1)
+                {
+                    array.set(i, compute_for_index(i))
+                }
+            }
+        })
+
+        var t3 = Thread(Runnable {
+            run {
+                for (i in (2*array.count()/4)..(3*(array.count()/4) - 1))
+                {
+                    array.set(i, compute_for_index(i))
+                }
+            }
+        })
+
+        var t4 = Thread(Runnable {
+            run {
+                for (i in (3*array.count()/4)..(array.count() - 1))
+                {
+                    array.set(i, compute_for_index(i))
+                }
+            }
+        })
+
+        t1.run()
+        t2.run()
+        t3.run()
+        t4.run()
+
+        t1.join()
+        t2.join()
+        t3.join()
+        t4.join()
+
+        for (i in 0..(array.count() - 1))
+        {
+            println(array[i])
+        }
+    })
+
+    println("Time :" + time)
 
 }
--- Kotlin/src/sequential.kt
+++ Kotlin/src/sequential.kt
@@ -1,10 +1,25 @@
+fun compute_for_index(index: Int) : Int 
+{
+    var res = 0
+    for (i in 0..index)
+    {
+        res += i
+    }
+    return res
+}
+
 fun main(args:Array<String>){
 
     var array = Array(1000000){0} //Initialise un tableau de 1M de cellules à 0
 
-    for (i in 0..(array.count() - 1))
-    {
-        array.set(i, i * i)
-        println(array[i])
-    }
+    val time = measureMillis({
+        for (i in 0..(array.count() - 1))
+        {
+            array.set(i, compute_for_index(i))
+            println(array[i])
+        }
+    })
+
+    println("Time :" + time)
+    
 }
