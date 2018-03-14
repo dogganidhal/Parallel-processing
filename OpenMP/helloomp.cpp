6dc7bd749eeda22cffc5262e9c93dbb33201d789
Nidhal DOGGA <dogga.nidhal@gmail.com>
2018-03-14 15:06:40 +0100

    successful omp hello World

--- OpenMP/helloomp.cpp
+++ OpenMP/helloomp.cpp
@@ -3,8 +3,13 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstdio>
+#include <ctime>
 #include <omp.h>
 #include "sha512.hpp"
+
+#include <chrono>
+typedef std::chrono::high_resolution_clock Clock;
 
 using namespace std;
 
@@ -18,25 +23,59 @@ int main(int argc, char *argv[])
 {
 
     const size_t SIZE = 1000000;
-    const size_t section_count = 5;
+    const size_t section_count = 4;
     const size_t values_per_section = SIZE / section_count;
 
-    string hashes[SIZE];
+    vector<string> hashes;
+    hashes.reserve(SIZE);
     
-    #pragma omp parallel
+    auto begin = Clock::now();
+
+    if (argc == 2 && strcmp(argv[1], "-p") == 0)
     {
-        #pragma omp sections
+        #pragma omp parallel
         {
-            for (size_t i = 0; i < section_count; i++)
+            #pragma omp sections
             {
                 #pragma omp section
-                for (size_t index = i * values_per_section; index < (i + 1) * values_per_section; index++)
+                for (size_t index = 0; index < values_per_section; index++)
+                {
                     hashes[index] = sha512(string_for_index(index));
+                }
+                #pragma omp section
+                for (size_t index = values_per_section; index < 2 * values_per_section; index++)
+                {
+                    hashes[index] = sha512(string_for_index(index));
+                }
+                #pragma omp section
+                for (size_t index = 2 * values_per_section; index < 3 * values_per_section; index++)
+                {
+                    hashes[index] = sha512(string_for_index(index));
+                }
+                #pragma omp section
+                for (size_t index = 3 * values_per_section; index < 4 * values_per_section; index++)
+                {
+                    hashes[index] = sha512(string_for_index(index));
+                }
             }
         }
+    } else 
+    {
+        for (size_t i = 0; i < section_count; i++)
+        {
+            for (size_t index = i * values_per_section; index < (i + 1) * values_per_section; index++)
+                hashes[index] = sha512(string_for_index(index));
+        }
     }
-    for (size_t i = 0; i < SIZE ; i ++)
-        cout << hashes[i] << endl;
+
+    
+
+    auto end = Clock::now();
+
+    // for (size_t i = 0; i < SIZE ; i ++)
+    //     cout << hashes[i] << endl;
+
+    cout << "Time elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << endl;
 
     return EXIT_SUCCESS;
 }
