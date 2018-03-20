60a39ee8800ea5fd017feaa7c21d0f379e50a578
Driss Gddm <driss.gueddoum@etu.unistra.fr>
2018-03-20 21:51:53 +0100

    sa mère la grosse

--- Pthreads/main.c
+++ Pthreads/main.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-//#include <pthread.h>
+#include <pthread.h>
 
 void usage (void)
 {
@@ -19,7 +19,7 @@ int recherche(long int* const tab, int a, int b, int x)
 {
 	if (a >= b) return a;
 	int m = (a + b) / 2;
-	if (tab[m]<x) return recherche(tab, m + 1, b, x);
+	if (tab[m] < x) return recherche(tab, m + 1, b, x);
 	else return recherche(tab, a, m, x);
 }
 void inserer(long int* const tab, int n, int pos, int x)
@@ -38,7 +38,6 @@ long int* custom_sort(size_t size, long int* array)
 	  int pos = recherche(result, 0, i, array[i]);
 	  inserer(result, i, pos, array[i]);
   }
-  //free(array);
   return result;
 }
 
@@ -54,6 +53,16 @@ void print_array(long int *array, int length)
     printf("%ld \n", array[i]);
 }
 
+void *computation(void* arg)
+{
+        int i, size = *((int*) arg);
+        long int array[size];
+        for (i = 0; i <= size; i++)
+            array[i] = random_val();
+        
+        long int *sorted_array = custom_sort(size, array);
+}
+
 int main (int argc, char *argv[])
 {
     srand((unsigned)time(NULL));
@@ -63,18 +72,33 @@ int main (int argc, char *argv[])
         usage();
     else if(strcmp(argv[1], "-s") == 0)
     {
-        printf("Sequential way... \n");
-        int i, size = atoi(argv[2]);
-        long int array[size];
-        for (i = 0; i <= size; i++)
-            array[i] = random_val();
-        long int *sorted_array = custom_sort(size, array);
+    printf("Sequential way... \n");
+    int size = atoi(argv[2]);
+    computation(&size);
     }
     else if(strcmp(argv[1], "-p") == 0)
     {
         printf("Parallel way... \n");
+        int nb_threads = 4;
+        pthread_t thread[nb_threads - 1] = {pthread_t thread1, pthread_t thread5, pthread_t thread3, pthread_t thread4};
+        int i, size = atoi(argv[2]);
+        for (i = 0; i < nb_threads; i++)
+        {
+            if(pthread_create(&thread[i], NULL, computation, &size) == -1)
+            {
+	            perror("pthread_create");
+	            return EXIT_FAILURE;
+            }
+        }
+        for (i = 0; i < nb_threads; i++)
+        { 
+            if(pthread_join(thread[i], NULL)) 
+            {
+	            perror("pthread_join");
+	            return EXIT_FAILURE;
+            }
+        }
     }
-
     clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
     printf("Time : %lu ms \n", (stop.tv_sec - start.tv_sec) * 1000 + (stop.tv_nsec - start.tv_nsec) / 1000000);
     return EXIT_SUCCESS;
