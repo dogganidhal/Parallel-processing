35b6fe1d86793e43d79e21728d91ff1d96ce0c82
Nidhal DOGGA <dogga.nidhal@gmail.com>
2018-03-27 19:28:39 +0200

    xfind

---
+++ OpenMP/xfind.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+
+int main(int argc, char *argv[])
+{
+
+    printf("Hello from XFIND\n");
+
+    return EXIT_SUCCESS;
+}
